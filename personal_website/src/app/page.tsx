0dabc475980ae816f51d80daae1316391035509c
jakeduffy <duffyjemmett@gmail.com>
2023-07-03 02:01:55 -0500

    initialize app

---
+++ personal_website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main background="white">
+      <div background="white">
+        What is going on?
+      </div>
+    </main>
+  )
+}
